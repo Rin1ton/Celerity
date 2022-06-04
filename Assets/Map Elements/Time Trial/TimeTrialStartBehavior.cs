a0ddf577a4f36cbf4aab0c39210890c021efd8aa
The_Shadow1098 <64919019+Rin1ton@users.noreply.github.com>
2022-06-04 15:05:19 -0400

    Organized Level Hierarchy/ Work on Time Trial

---
+++ Assets/Map Elements/Time Trial/TimeTrialStartBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTrialStartBehavior : MonoBehaviour
+{
+
+    public GameObject myLeftPillarHead;
+    public GameObject myRightPillarHead;
+    public GameObject myCrossingLine;
+    Renderer myLeftPillarHeadRenderer;
+    Renderer myRightPillarHeadRenderer;
+    Renderer myCrossingLineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLeftPillarHeadRenderer = myLeftPillarHead.GetComponent<Renderer>();
+        myRightPillarHeadRenderer = myRightPillarHead.GetComponent<Renderer>();
+        myCrossingLineRenderer = myCrossingLine.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
