701085bf1d8cc8dc8529fd618a5e30839d68a606
Vinni Horstkotte <64919019+Rin1ton@users.noreply.github.com>
2024-03-01 14:10:24 -0500

    A* pathing implemented

--- Assets/Villages/Node.cs
+++ Assets/Villages/Node.cs
@@ -6,10 +6,19 @@ public class Node
 {
     public bool walkable;
     public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
 
-    public Node(bool _walkable, Vector3 _worldPos)
+    public int gCost;
+    public int hCost;
+    public int fCost => gCost + hCost;
+    public Node parent;
+
+    public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY)
     {
         walkable = _walkable;
         worldPosition = _worldPos;
+        gridX = _gridX;
+        gridY = _gridY;
     }
 }
