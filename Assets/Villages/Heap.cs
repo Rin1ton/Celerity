3511fe0c08eefac6f4236516abc551b43fe7d90e
Rin1ton <n00i3.n4.i.0reooo@gmail.com>
2024-03-17 13:48:26 -0400

    started working on heap implementation

---
+++ Assets/Villages/Heap.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heap<T> where T : IHeapItem<T>
+{
+	T[] items;
+	int currentItemCount;
+
+	public Heap(int maxHeapSize)
+	{
+		items = new T[maxHeapSize];
+	}
+
+	public void Add(T item)
+	{
+		item.HeapIndex = currentItemCount;
+		items[currentItemCount] = item;
+
+		SortUp(item);
+		currentItemCount++;
+	}
+
+	void SortUp(T item)
+	{
+		int parentIndex = (item.HeapIndex - 1) / 2;
+	}
+}
+
+public interface IHeapItem<T> : IComparable<T>
+{
+	int HeapIndex
+	{
+		get;
+		set;
+	}
+}
